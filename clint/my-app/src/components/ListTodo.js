a1a11076284ce46de1dd2366e6b3f3318a2760ec
shubham-patil99 <shubham.patil9964123@gmail.com>
2023-04-10 17:35:42 +0530

    added delete functionality

--- clint/my-app/src/components/ListTodo.js
+++ clint/my-app/src/components/ListTodo.js
@@ -1,11 +1,39 @@
 import React, {useEffect, useState} from "react";
 
-
-
-
 const ListTodos =()=>{
-    return(
-        <div className="mt-3">
+
+const [todos, setTodos] = useState([])  
+
+const deleteTodo = async (id) => {
+  try {
+    const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`,{
+      method: "DELETE"
+    })
+
+    setTodos(todos.filter(todo=>todo.todo_id !==id))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const getTodos = async () => {
+  try{
+    const response = await fetch("http://localhost:5000/todos")
+    const jsonData = await response.json();
+
+    setTodos(jsonData);
+  }catch(err){
+    console.error(err.message);
+  }
+}
+
+useEffect(()=>{
+  getTodos();
+},[])
+
+ console.log(todos)
+return(
+<div className="mt-3">
 <table className="table">
     <thead>
       <tr>
@@ -15,11 +43,13 @@ const ListTodos =()=>{
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
+      {todos.map(todos =>(
+        <tr key={todos.todo_id}>
+        <td>{todos.description}</td>
+        <td><button className="btn btn-primary">Edit</button></td>
+        <td><button className="btn btn-danger" onClick={()=>deleteTodo(todos.todo_id)}>Delete</button></td>
+        </tr>
+      ))}
     </tbody>
   </table>
         </div>
